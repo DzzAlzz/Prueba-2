9c47579a6ce76864b224a5541995db39324a7785
DzzAlzz <xoptiicx7@gmail.com>
2026-05-28 10:31:00 -0400

    Creacion de endpoint GET productos por ID

--- routes/productos.routes.js
+++ routes/productos.routes.js
@@ -5,5 +5,12 @@ const router = express.Router();
 router.get("/productos", (req, res) => {
     res.send("Lista de productos");
 });
+router.get("/productos/:id", (req, res) => {
+
+    const id = req.params.id;
+
+    res.send("Producto ID: " + id);
+
+});
 
 module.exports = router;
