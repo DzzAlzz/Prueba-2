37cae329008aba293f794512dc81e2efe68e3cba
DzzAlzz <xoptiicx7@gmail.com>
2026-05-28 11:36:14 -0400

    Agregado estilo basico al formulario

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const productosRoutes = require("./routes/productos.routes");
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 app.use(cookieParser());
 
 
